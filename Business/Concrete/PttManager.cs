e3eb75b288b2c5d108a44e015497d8acfca89120
Yusuf Akin <yusuf.akn2017@hotmail.com>
2024-03-05 10:13:42 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager(IApplicantService applicantService) : ISupplierService
+    {
+        public void GiveMask(Person person)
+        { 
+            if (applicantService.CheckPerson(person))
+            {
+                Console.WriteLine("Vatandaş Bilgileri doğrulandı");
+                Console.WriteLine(person.Name + " için maske verildi!");
+            }
+            else
+            {
+                Console.WriteLine("Vatandaş Bilgileri doğrulanamadı!");
+                Console.WriteLine(person.Name + " için maske verilemedi!");
+            }
+        }
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace WorkAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var person = GetInformationWithConsole();
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
+            
+        }
+
+        private static Person GetInformationWithConsole()
+        {
+            Console.WriteLine("Lütfen bilgilerinizi girin:");
+
+            Console.Write("İsim: ");
+            string isim = Console.ReadLine();
+
+            Console.Write("Soyisim: ");
+            string soyisim = Console.ReadLine();
+
+            Console.Write("Doğum Yılı: ");
+            int dogumYili;
+            while (!int.TryParse(Console.ReadLine(), out dogumYili))
+            {
+                Console.WriteLine("Geçersiz yaş. Lütfen bir sayı girin.");
+                Console.Write("Doğum Yılı: ");
+            }
+
+            Console.Write("TC Kimlik No: ");
+            long tcNo;
+            while (!long.TryParse(Console.ReadLine(), out tcNo))
+            {
+                Console.WriteLine("Geçersiz TC Kimlik No. Lütfen bir sayı girin.");
+                Console.Write("TC Kimlik No: ");
+            }
+            Person person = new Person();
+            person.Name = isim;
+            person.Surname = soyisim;
+            person.DateofBirthYear = dogumYili;
+            person.NationalIdentity = tcNo;
+            return person;
+        }
+    }
+}
+
+
